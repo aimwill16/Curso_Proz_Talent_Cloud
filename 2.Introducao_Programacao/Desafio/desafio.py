9b4becab4a38ffaf88dfadd74a10f1bfcda7d083
aimwill16 <williancosta16@gmail.com>
2023-08-10 17:52:57 -0300

    correcao na funcao de remover

--- 2.Introducao_Programacao/Desafio/desafio.py
+++ 2.Introducao_Programacao/Desafio/desafio.py
@@ -2,34 +2,36 @@
 
 produtos = ["Abacate","Maça","Banana","Uva"]
 
-def mostra_produto(produto):
-    for item in produto:
-        print(item, end=' ')
+def mostra_produto(itens):
+    for i in itens:
+        print(i, end=" ")
 def adiciona_item(produtos):
     adc = produtos.append(input("Digite o produto para acrescentar: \n"))
 
 def remove_produto(produtos):
     rm = input("Digite o produto para remover: \n")
-    try:
-        for i in produtos:
-            if i == rm:
-                produtos.remove(rm)
-                print(f"Produto '{rm}' removido.")
-    except:
-        print("Produto não encontrado")
+    for i in produtos:
+        if i == rm:
+            produtos.remove(rm)
+            print(f"Produto '{rm}' removido.")
+    if i != rm:
+        print(f"Produto '{rm}' não encontrado")
 
 print("===   Gerenciador de Estoque - Frutas   ====")
 verificador = False
 try:
     while verificador == False:
-        opt = int(input("\n\nDigite o numero da opção: \n"
+        opt = int(input("\nOpções:\n"
                         "1. Mostrar itens em estoque.\n"
                         "2. Adicionar itens em estoque.\n"
                         "3. Remover itens em estoque.\n"
-                        "4. Sair do programa\n"))
+                        "4. Sair do programa\n"
+                        "\nDigite a opção desejada:\n"))
 
         if opt == 1:
+            print("Os itens atuais são:")
             mostra_produto(produtos)
+            print("")
         elif opt == 2:
             adiciona_item(produtos)
         elif opt == 3:
